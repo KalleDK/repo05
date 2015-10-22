76ef8a66ca32ddb7631efb6060fc9448d080bbf2
KasperSK <raizzat@gmail.com>
2015-10-22 12:50:26 +0200

    Initial project setup

---
+++ ATM/ATM/Events/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Models;
+
+namespace ATM.Events
+{
+    interface IEventHandler
+    {
+        void CheckForEvent(List<Plane> activePlanes);
+    }
+}
---
+++ ATM/ATM/Models/Plane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Models
+{
+    class Plane
+    {
+        string Tag { get; set; }
+
+        Coordinate[] Position { get; set; }
+
+        double? Speed { get; set; }
+
+        double? Compass { get; set; }
+    }
+
+    struct Coordinate
+    {
+        int x { get; set; }
+        int y { get; set; }
+        int z { get; set; }
+    }
+}
