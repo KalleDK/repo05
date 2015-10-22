25e320c4827db506661ca36a2d23dc75cab4daaa
Kalle Møller <github.com@k-moeller.dk>
2015-10-22 13:22:23 +0200

    Logging interface added

---
+++ ATM/ATM/Logging/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ATM.Logging
+{
+    public interface ILogger
+    {
+        void Info(string line);
+        void Warning(string line);
+    }
+}
