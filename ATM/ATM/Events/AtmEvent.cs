edc59006addd97c5a83365adbc69565e2ef30e35
KasperSK <raizzat@gmail.com>
2015-10-28 12:12:09 +0100

    EventPoly

--- ATM/ATM/Events/AtmEvent.cs
+++ ATM/ATM/Events/AtmEvent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ATM.Events
@@ -25,6 +26,32 @@ namespace ATM.Events
         public Category EventCatagory;
         public DateTime Timesstamp;
         public List<string> Tags = new List<string>();
+
+    }
+
+    public abstract class AtmEventBase
+    {
+        protected IEventController EventController;
+        public string EventCatagory { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+
+    }
+
+    public class AtmEventTimed : AtmEventBase
+    {
+        public int Timeout;
+     }
+
+    public class AtmEventEnter : AtmEventTimed
+    {
+        public string Tag { get; set; }
+    }
+
+
+    public class AtmEventSeperation : AtmEventBase
+    {
+        public List<string> Tags { get; set; }
 
     }
 }
