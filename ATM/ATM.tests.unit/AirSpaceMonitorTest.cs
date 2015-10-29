9b3cb7e404521f301e4c5d40614dc87a8772ad45
Kalle Møller <github.com@k-moeller.dk>
2015-10-29 13:16:25 +0100

    AirSpaceMonitorTests

--- ATM/ATM.tests.unit/AirSpaceMonitorTest.cs
+++ ATM/ATM.tests.unit/AirSpaceMonitorTest.cs
@@ -3,46 +3,28 @@ using ATM.AirSpace;
 using ATM.Models;
 using NUnit.Framework;
 
-/*
+
 namespace ATM.tests.unit
 {
     [TestFixture]
-
-    
-
-   
-
     public class AirSpaceMonitorTest
     {
         private AirSpaceMonitor uut;
 
-        private Plane Testplane_In;
-
-        private Plane Testplane_Out;
-
-        private Coordinate _min;
-
-        private Coordinate _max;
-
         [SetUp]
         public void SetUp()
         {
-            _min = new Coordinate() {X = 1000, Y = 1000, Z = 10};
+            var minCoordinate = new Coordinate {X = 1000, Y = 1000, Z = 10};
 
-            _max = new Coordinate() {X = 10000, Y = 10000, Z=100};
+            var maxCoordinate = new Coordinate {X = 10000, Y = 10000, Z=100};
 
-            Testplane_In = new Plane() {Compass = null, Position = new Coordinate[1], Speed = null, Tag = "TestFly Inde"};
+            var airspace = new AirspaceModel(minCoordinate,maxCoordinate);
 
-            Testplane_Out = new Plane() { Compass = null, Position = new Coordinate[1], Speed = null, Tag = "TestFly Ude" };
-
-            Testplane_In.Position[0] = new Coordinate() {X=1200, Y=4000, Z=42};
-
-            Testplane_Out.Position[0] = new Coordinate() {X=900, Y=1000, Z=10};
-
-            uut = new AirSpaceMonitor() {Max = _max, Min= _min};
-
+            uut = new AirSpaceMonitor(airspace);
         }
 
+        [Test]
+        public void AirSpaceMonitor_Constructor_Correct
 
         [Test]
         public void AirSpaceMonitor_CheckAirSpace_ListDoesNotContainPlaneout()
@@ -65,4 +47,4 @@ namespace ATM.tests.unit
     }
 
 
-}*/
+}
